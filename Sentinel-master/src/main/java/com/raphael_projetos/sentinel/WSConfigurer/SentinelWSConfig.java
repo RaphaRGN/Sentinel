a9a77e432bff2e371cad7d9656d8f4a6385c9e84
raphael.netoHSC <raphaelgoneto@gmail.com>
2024-08-07 15:05:54 -0300

    Commit de teste no HSC

---
+++ Sentinel-master/src/main/java/com/raphael_projetos/sentinel/WSConfigurer/SentinelWSConfig.java
@@ -0,0 +1,30 @@
+package com.raphael_projetos.sentinel.WSConfigurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SentinelWSConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+    registry.addEndpoint("/ws-admin")
+            .setAllowedOrigins("http://localhost:8081","http://localhost:8080")
+            .withSockJS();
+
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/topic");
+
+    }
+
+
+
+}
