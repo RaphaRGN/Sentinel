106e65e98380dea085bd9399639759ff861611c5
raphael.netoHSC <raphaelgoneto@gmail.com>
2024-08-07 15:05:54 -0300

    Commit de teste no HSC

---
+++ Sentinel-master/src/test/java/com/raphael_projetos/sentinel/SentinelApplicationTests.java
@@ -0,0 +1,14 @@
+package com.raphael_projetos.sentinel;
+
+import com.raphael_projetos.sentinel.Application.SentinelApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest (classes = SentinelApplication.class)
+class SentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
